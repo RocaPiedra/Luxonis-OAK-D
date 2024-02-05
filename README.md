09079abfc51c81cf10b9f7a849f70935612b156f
Pablo Roca <pablo_rocasg97@hotmail.com>
2024-02-05 13:31:22 +0100

    new hole detector class with calibration data and updated RGB pipeline

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Luxonis-OAK-D
 Software for testing of Luxonis OAK-D product family
 
+## Canny edge detection
+For preprocessing purposes the contours will be extracted after applying the Canny algorithm to the image. This will produce a binary image that only shows the edges between the thresholds established. So far we've tested:
+- Only Canny directly to RGB image: Rarely misses the target but produces tons of shapes in return
+
+
+- Preprocessing (Grayscale + blur with 7x7 kernel) + Canny + Erosion + Dilation: Sometimes filters target hexagon but the image has less artifacts.
+  - Succesful testing with low canny threshold (< 50) and normal high canny threshold (300)
+
 ## Important Notes
 [Spatial Location Calculator software](https://docs.luxonis.com/projects/api/en/latest/samples/SpatialDetection/spatial_location_calculator/#spatial-location-calculator) tested with OAK-D Pro without laser gives accurate measurements above 317 mm distance. Far distances accuracy is not clear but nonetheless out of scope for current applications.
--- ShapeDetector/luxonis_geometry_detector.py
+++ ShapeDetector/luxonis_geometry_detector.py
@@ -7,79 +7,162 @@ import cv_functions
 import calibrator_class
 import imutils
 from shape_detector import ShapeDetector
+from dataclasses import dataclass
 
-def frameNorm(frame, bbox):
-    normVals = np.full(len(bbox), frame.shape[0])
-    normVals[::2] = frame.shape[1]
-    return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
+@dataclass
+class Resolution:
+    width: int = 1280
+    height: int = 720
 
-def showPipeline_RGB(pipeline: depthai.Pipeline, _cam_rgb_pipeline: depthai.node.ColorCamera):
+# default values for canny algorithm with good results
+@dataclass
+class CannyCalibration:
+    low: int = 25
+    high: int = 241
     
-    xout_rgb = pipeline.create(depthai.node.XLinkOut)
-    xout_rgb.setStreamName("rgb")
-    _cam_rgb_pipeline.preview.link(xout_rgb.input)
-    with depthai.Device(pipeline) as device:
-        q_rgb = device.getOutputQueue("rgb")
-        frame = None        
-        while True:
-            in_rgb = q_rgb.tryGet()            
-            if in_rgb is not None:
-                frame = in_rgb.getCvFrame()                
-                if frame is not None:
-                    cv2.imshow("preview", frame)
-                    
-            if cv2.waitKey(1) == ord('q'):
-                break
-            
-            
-def showPipeline_Depth(pipeline: depthai.Pipeline, _cam_pipeline: depthai.node.StereoDepth):
-    
-    xout_rgb = pipeline.create(depthai.node.XLinkOut)
-    xout_rgb.setStreamName("depth")
-    _cam_pipeline.preview.link(xout_rgb.input)
-    with depthai.Device(pipeline) as device:
-        q_rgb = device.getOutputQueue("depth")
-        frame = None        
-        while True:
-            in_rgb = q_rgb.tryGet()            
-            if in_rgb is not None:
-                frame = in_rgb.getCvFrame()                
-                if frame is not None:
-                    cv2.imshow("depth", frame)
-                    
-            if cv2.waitKey(1) == ord('q'):
-                break
-            
-def prepareRGBPipeline(pipeline, cam_rgb):    
-    
-    cam_rgb.setPreviewSize(1280, 720)
-    cam_rgb.setInterleaved(False)
-    xout_rgb = pipeline.create(depthai.node.XLinkOut)
-    xout_rgb.setStreamName("rgb")
-    cam_rgb.preview.link(xout_rgb.input)
-    return 
 
-def getShapes(preprocessed_img, target_shape = None):    
-    if preprocessed_img is not None:
-        cnts = cv2.findContours(preprocessed_img.copy(), cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE)	
-        cnts = imutils.grab_contours(cnts)	# check utility
-        sd = ShapeDetector()
-        # loop over the contours
-        for c in cnts:
-            if c is not None:
-                sd.get_contour(c, 1, preprocessed_img, target_shape)
+
+class HoleDetector():        
+    
+    def __init__(self, rgb_res: Resolution = Resolution(1280,720), depth_res: Resolution = Resolution(1280,720)):
+        self.pipeline = depthai.Pipeline()
+        # prepare rgb stream for edge detection     
+        self.cam_rgb = self.pipeline.create(depthai.node.ColorCamera)
         
-        return preprocessed_img
+        self.prepareRGBPipeline(rgb_res)
+        
+        self.shape_detector = ShapeDetector()
+        
+        self.kernel = np.ones((5,5),np.uint8)
+
+    def frameNorm(frame, bbox):
+        normVals = np.full(len(bbox), frame.shape[0])
+        normVals[::2] = frame.shape[1]
+        return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
+
+    def showPipeline_RGB(pipeline: depthai.Pipeline, _cam_rgb_pipeline: depthai.node.ColorCamera):        
+        xout_rgb = pipeline.create(depthai.node.XLinkOut)
+        xout_rgb.setStreamName("rgb")
+        _cam_rgb_pipeline.preview.link(xout_rgb.input)
+        with depthai.Device(pipeline) as device:
+            q_rgb = device.getOutputQueue("rgb")
+            frame = None        
+            while True:
+                in_rgb = q_rgb.tryGet()            
+                if in_rgb is not None:
+                    frame = in_rgb.getCvFrame()                
+                    if frame is not None:
+                        cv2.imshow("preview", frame)
+                        
+                if cv2.waitKey(1) == ord('q'):
+                    break
+                
+    def showPipeline_Depth(pipeline: depthai.Pipeline, _cam_pipeline: depthai.node.StereoDepth):
+        
+        xout_rgb = pipeline.create(depthai.node.XLinkOut)
+        xout_rgb.setStreamName("depth")
+        _cam_pipeline.preview.link(xout_rgb.input)
+        with depthai.Device(pipeline) as device:
+            q_rgb = device.getOutputQueue("depth")
+            frame = None        
+            while True:
+                in_rgb = q_rgb.tryGet()            
+                if in_rgb is not None:
+                    frame = in_rgb.getCvFrame()                
+                    if frame is not None:
+                        cv2.imshow("depth", frame)
+                        
+                if cv2.waitKey(1) == ord('q'):
+                    break
+                
+    def prepareRGBPipeline(self, res: Resolution):    
+        
+        self.cam_rgb.setPreviewSize(res.width, res.height)
+        self.cam_rgb.setInterleaved(False)
+        self.xout_rgb = self.pipeline.create(depthai.node.XLinkOut)
+        self.xout_rgb.setStreamName("rgb")
+        self.cam_rgb.preview.link(self.xout_rgb.input)
+        return
+
+    def prepareSpatialLocationCalculator(self, xOutDepth, xoutSpatialData, xinSpatialCalcConfig):
+            # Define sources and outputs
+        monoLeft = self.pipeline.create(depthai.node.MonoCamera)
+        monoRight = self.pipeline.create(depthai.node.MonoCamera)
+        stereo = self.pipeline.create(depthai.node.StereoDepth)
+        spatialLocationCalculator = self.pipeline.create(depthai.node.SpatialLocationCalculator)
+
+        xOutDepth = self.pipeline.create(depthai.node.XLinkOut)
+        xoutSpatialData = self.pipeline.create(depthai.node.XLinkOut)
+        xinSpatialCalcConfig = self.pipeline.create(depthai.node.XLinkIn)
+
+        xOutDepth.setStreamName("depth")
+        xoutSpatialData.setStreamName("spatialData")
+        xinSpatialCalcConfig.setStreamName("spatialCalcConfig")
+
+        # Properties
+        monoLeft.setResolution(depthai.MonoCameraProperties.SensorResolution.THE_400_P)
+        monoLeft.setCamera("left")
+        monoRight.setResolution(depthai.MonoCameraProperties.SensorResolution.THE_400_P)
+        monoRight.setCamera("right")
+
+        stereo.setDefaultProfilePreset(depthai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+        stereo.setLeftRightCheck(True)
+        stereo.setSubpixel(True)
+
+        # Config
+        topLeft = depthai.Point2f(0.4, 0.4)
+        bottomRight = depthai.Point2f(0.6, 0.6)
+
+        config = depthai.SpatialLocationCalculatorConfigData()
+        config.depthThresholds.lowerThreshold = 100
+        config.depthThresholds.upperThreshold = 10000
+        calculationAlgorithm = depthai.SpatialLocationCalculatorAlgorithm.MEDIAN
+        config.roi = depthai.Rect(topLeft, bottomRight)
+
+        spatialLocationCalculator.inputConfig.setWaitForMessage(False)
+        spatialLocationCalculator.initialConfig.addROI(config)
+
+        # Linking
+        monoLeft.out.link(stereo.left)
+        monoRight.out.link(stereo.right)
+
+        spatialLocationCalculator.passthroughDepth.link(xOutDepth.input)
+        stereo.depth.link(spatialLocationCalculator.inputDepth)
+
+        spatialLocationCalculator.out.link(xoutSpatialData.input)
+        xinSpatialCalcConfig.out.link(spatialLocationCalculator.inputConfig)
+
+    def getShapes(self, preprocessed_img, rgb_img, target_shape = "hexagon", ratio = 1):    
+        if preprocessed_img is not None:
+            cnts = cv2.findContours(preprocessed_img.copy(), cv2.RETR_EXTERNAL,
+                cv2.CHAIN_APPROX_SIMPLE)	
+            cnts = imutils.grab_contours(cnts)	# check utility
+            shapeList = []
+            # loop over the contours
+            for c in cnts:
+                if c is not None:
+                    shapeList.append(self.shape_detector.get_contour(c, ratio, rgb_img, target_shape))           
+            
+            return rgb_img
+
+    def Preproc4ShapeDetection(self, img, canny_low_thresh, canny_high_thresh):
+        assert img is not None
+            
+        imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
+        imgCanny = cv2.Canny(imgBlur,canny_low_thresh,canny_high_thresh)
+        imgDilation = cv2.dilate(imgCanny,self.kernel,iterations=1)
+        imgEroded = cv2.erode(imgDilation,self.kernel,iterations=1)
+        
+        return imgEroded
 
 def main():
     
-    cannyCalibrator = calibrator_class.Calibrator(200,300,400,0,500,200)
+    cannyCalibrator = calibrator_class.Calibrator(CannyCalibration.low,CannyCalibration.high,400,0,500,200)
     cv_functions.createWindowTrackbar(cannyCalibrator)
-    pipeline = depthai.Pipeline()
-    cam_rgb = pipeline.create(depthai.node.ColorCamera)
-    prepareRGBPipeline(pipeline, cam_rgb)
-    with depthai.Device(pipeline) as device:
+    detector = HoleDetector(Resolution(1280,720))
+    
+    with depthai.Device(detector.pipeline) as device:
         q_rgb = device.getOutputQueue("rgb")
         frame = None        
         while True:
@@ -87,12 +170,15 @@ def main():
             if in_rgb is not None:
                 frame = in_rgb.getCvFrame()                
                 if frame is not None:
-                    cv2.imshow("preview", frame)
-                    imgCanny = cv2.Canny(frame,
-                                        cannyCalibrator.low_threshold,
-                                        cannyCalibrator.high_threshold)
-                    cv2.imshow("Canny", imgCanny)
-                    imgShapes = getShapes(imgCanny, "hexagon")
+                    # cv2.imshow("preview", frame)
+                    # imgCanny = cv2.Canny(frame,
+                    #                     cannyCalibrator.low_threshold,
+                    #                     cannyCalibrator.high_threshold)
+                    imgPreproc = detector.Preproc4ShapeDetection(frame, 
+                                                    cannyCalibrator.low_threshold,
+                                                    cannyCalibrator.high_threshold)
+                    cv2.imshow("Canny", imgPreproc)
+                    imgShapes = detector.getShapes(imgPreproc, frame)
                     cv2.imshow("Shapes", imgShapes)
                     
             if cv2.waitKey(1) == ord('q'):
